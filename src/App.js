1fceeb13486730359c0e0f4fb467f3be7109c2d0
monu11296 <you@example.com>
2019-01-24 18:17:36 +0530

    Adding TextField and Using State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      phone: ''
+    };
+
+    this.nameChange = this.nameChange.bind(this);
+    this.phoneChange = this.phoneChange.bind(this);
+  }
+
+  nameChange =(e) =>{
+    this.setState({name: e.target.value});
+  }
+  phoneChange =(e) =>{
+    this.setState({phone: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Hello World!</p><br />
+        <p>Enter Your Name</p>
+        <input type="text" onChange={this.nameChange}></input><br /><br /><br /><br />
+        <input type="tel" onChange={this.phoneChange}></input>
+        <br />
+        <p>Your Name: {this.state.name}</p>
+        <p>Your Phone: {this.state.phone}</p>
       </div>
     );
   }
